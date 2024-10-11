065a22ef834d51861b19ab91c2aadd26cb0257de
dewantara tirta <dewantara.tirta@gmail.com>
2024-10-11 20:34:01 +0700

    feat(explorePage)

--- resources/js/lib/components/WalletList.svelte
+++ resources/js/lib/components/WalletList.svelte
@@ -12,25 +12,18 @@
             <h4 class="h4 font-bold">{title}</h4>
         {/if}
 
-        <div
-            class="snap-x scroll-px-4 snap-mandatory scroll-smooth flex gap-4 overflow-x-auto px-4 py-10"
-        >
-            {#each wallet as item, i}
-                <div
-                    class="snap-start shrink-0 card w-40 md:w-80 text-center md:flex md:flex-row md:grow md:justify-between"
-                >
-                    <!-- image -->
-                    <Link
-                        class="flex"
-                        href={"/app/profile/" + item.wallet_address}
-                    >
-                        <img src={item?.avatar_url} class="w-40" />
-                    </Link>
-
-                    <div>
-                        <p class="text-center">
-                            {item?.wallet_username}
-                        </p>
+        <div class="flex overflow-x-scroll pb-8 hide-scroll-bar">
+            {#each wallet as item}
+                <div class="flex flex-nowrap md:ml-10 ml-2">
+                    <div class="inline-block px-3">
+                        <Link href={`/app/profile/${item?.wallet_address}`}
+                            class="relative flex flex-col w-40 h-40 max-w-xs overflow-hidden bg-white transition-shadow duration-300 ease-in-out "
+                        >
+                            <div>
+                                <img src={item?.avatar_url} alt="" />
+                            </div>
+                            <p class="absolute left-0 bottom-0 text-center w-full">@{item?.wallet_username}</p>
+                        </Link>
                     </div>
                 </div>
             {/each}
